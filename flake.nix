96f9de3f38188dcecb22b9484fa07d4cf27cc800
kotatsuyaki <robinhuang123@gmail.com>
2022-06-17 14:21:10 +0800

    Add dependency on pandoc-amsthm

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
-  description = "A very basic flake";
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.utils.url = github:numtide/flake-utils;
 
@@ -10,10 +9,26 @@
           system = "x86_64-linux";
           config.allowUnfree = true;
         };
+        pandoc-amsthm = pkgs.python3Packages.buildPythonPackage {
+          pname = "pandoc-amsthm";
+          version = "2.0.0";
+          format = "pyproject";
+          src = pkgs.fetchFromGitHub {
+            owner = "ickc";
+            repo = "pandoc-amsthm";
+            rev = "v2.0.0";
+            sha256 = "sha256-hyXti/uM6y1HBZkbORgRh2HmY46vKH404647ktCRuL8=";
+          };
+          propagatedBuildInputs = with pkgs; [
+            poetry
+            python3Packages.panflute
+          ];
+        };
         deps = with pkgs; [
           texlive.combined.scheme-full
           pandoc
           haskellPackages.pandoc-crossref
+          pandoc-amsthm
           librsvg
           gnumake
         ];
